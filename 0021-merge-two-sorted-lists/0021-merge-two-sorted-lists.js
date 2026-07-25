b839b184bd6b95a7c8b79575045a1ae08e3267d9
Abdullah <abdullahvallanchira762@gmail.com>
2026-07-25 14:16:21 +0530

    Time: 0 ms (100%), Space: 57.4 MB (61.29%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,25 @@
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+const mergeTwoLists = function (list1, list2) {
+    let dummy = new ListNode(0);
+    let op = dummy;
+
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            op.next = list1;
+            list1 = list1.next;
+        } else {
+            op.next = list2;
+            list2 = list2.next;
+        }
+        op = op.next;
+    }
+    op.next = list1 || list2;
+
+    return dummy.next;
+};
